ed78194d181c6495b3e207158338965649c1e178
Loide Mara Verdes <mara.verdes@gmail.com>
2020-05-12 22:01:27 -0400

    Create script to convert xml to dataset

---
+++ convert_xml.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import xml.etree.ElementTree as et
+
+def parse_XML(xml_file, df_cols):
+    xtree = et.parse(xml_file)
+    xroot = xtree.getroot()
+    rows = []
+
+    for node in xroot:
+        if node.tag == "ProductLine_array":
+            res = []
+            for product_line in node:
+                #print(product_line[0].text, product_line[1].text)
+                rows.append({df_cols[0]: product_line[0].text, df_cols[1]: product_line[1].text})
+                #print(rows)
+
+    out_df = pd.DataFrame(rows, columns=df_cols)
+
+    return out_df
+
+def main():
+    filename = 'meliuz02-20-orders.xml'
+    #df_columns = ["GLProductGroupID", "ProductGroupName"]
+    df_columns = ["category_id", "description"]
+    output = parse_XML(filename, df_columns)
+    print(output.to_string())
+
+if __name__ == "__main__":
+    main()
