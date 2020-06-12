f647f6c9c556e8cf3f4f63f9e41213dc2749846a
Loide Mara Verdes <loide.verdes@meliuz.com.br>
2020-06-11 21:16:48 -0400

    Remove unnecessary stuff

--- convert_xml.py
+++ convert_xml.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import xml.etree.ElementTree as et
 
+"""
+Read a .xml file to process data and return a spreadsheet file result
+"""
+
 def parse_XML(xml_file, df_cols):
     xtree = et.parse(xml_file)
     xroot = xtree.getroot()
@@ -10,9 +14,7 @@ def parse_XML(xml_file, df_cols):
         if node.tag == "ProductLine_array":
             res = []
             for product_line in node:
-                #print(product_line[0].text, product_line[1].text)
                 rows.append({df_cols[0]: product_line[0].text, df_cols[1]: product_line[1].text})
-                #print(rows)
 
     out_df = pd.DataFrame(rows, columns=df_cols)
 
@@ -25,8 +27,7 @@ def export_to_excel(df):
     return df.to_excel(result_file, index=None, header=True)
 
 def main():
-    filename = 'meliuz02-20-orders.xml'
-    #df_columns = ["GLProductGroupID", "ProductGroupName"]
+    filename = 'random-report.xml'
     df_columns = ["category_id", "description"]
     output = parse_XML(filename, df_columns)
     print(output.to_string())
